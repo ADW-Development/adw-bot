7e32588794059da64a2d234e5333f2941a1d047c
T_cat9000_2 <terpstragraham@gmail.com>
2024-12-22 12:36:38 -0800

    fixed i think...

--- bot.py
+++ bot.py
@@ -3,17 +3,17 @@ import requests
 URLchecks = []
 
 '''
-Add urls that need to be checked here.
-example: https://adw-development.github.io/newpage
+Add URLs that need to be checked here.
+Example: https://adw-development.github.io/newpage
 '''
 
-print("conducting url checks...")
+print("Conducting URL checks...")
 
 for url in URLchecks:
     try:
         main_response = requests.get(url)
         if main_response.status_code == 200:
-            print(f"The page: {url} exists")
+            print(f"The URL: {url} exists")
         else:
             print(f"The page: {url} has an error. Check the HTML or CSS. HTTP status: {main_response.status_code}")
     except requests.exceptions.RequestException as error:
