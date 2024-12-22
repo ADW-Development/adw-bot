5cd4a75845a6ac50036974e06b609ac087633fd3
T_cat9000_2 <terpstragraham@gmail.com>
2024-12-22 12:33:24 -0800

    bot

--- bot.py
+++ bot.py
@@ -13,8 +13,8 @@ for url in URLchecks:
     try:
         main_response = requests.get(url)
         if main_response.status_code == 200:
-            print(f"The URL: {url} exists")
+            print(f"The page: {url} exists")
         else:
-            print(f"The page: {url} has an error. check the html or css, http status: {main_response.status_code}")
+            print(f"The page: {url} has an error. Check the HTML or CSS. HTTP status: {main_response.status_code}")
     except requests.exceptions.RequestException as error:
-        print(f"An error occurred while checking the url: {error}")   
+        print(f"An error occurred while checking the URL: {error}")
