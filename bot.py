b16e6654586623e835adeb9d831850676678dc4a
T_cat9000_2 <terpstragraham@gmail.com>
2025-01-31 15:16:24 -0800

    update the bot

--- bot.py
+++ bot.py
@@ -15,6 +15,6 @@ for url in URLchecks:
         if main_response.status_code == 200:
             print(f"The URL: {url} exists")
         else:
-            print(f"The page: {url} has an error. Check the HTML or CSS. HTTP status: {main_response.status_code}")
+            print(f"The page: {url} has an error. HTTP status: {main_response.status_code}")
     except requests.exceptions.RequestException as error:
         print(f"An error occurred while checking the URL: {error}")
